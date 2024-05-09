cf7bbbf5f315ecbd3156fcc8dbe40e5df768662f
sergi.moliner <sergilopezmoliner@gmail.com>
2024-05-09 10:56:23 +0200

    scroll button

--- src/app/starships-list/starships-list.component.ts
+++ src/app/starships-list/starships-list.component.ts
@@ -1,31 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { StarshipsService } from '../services/starships.service';
-import { Observable, EMPTY, catchError, BehaviorSubject, concatMap, finalize, from } from 'rxjs';
+import { Observable, EMPTY, catchError, finalize, fromEvent, concatMap } from 'rxjs';
 import { StarshipResults } from '../core/interfaces/starship';
 import { StarshipInfoComponent } from '../starship-info/starship-info.component';
 import { ErrorMessageComponent } from '../error-message/error-message.component';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
-import { scan, startWith } from 'rxjs/operators';
-import { filter } from 'rxjs/operators';
-import { map } from 'rxjs/operators';
-import { fromEvent } from 'rxjs';
-import { ViewChild, ElementRef } from '@angular/core';
-import { tap } from 'rxjs/operators';
-
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-starships-list',
   standalone: true,
-  imports: [AsyncPipe, StarshipInfoComponent, ErrorMessageComponent, InfiniteScrollModule],
+  imports: [AsyncPipe, StarshipInfoComponent, ErrorMessageComponent, InfiniteScrollModule, CommonModule],
   templateUrl: './starships-list.component.html',
   styleUrl: './starships-list.component.scss'
 })
 export class StarshipsListComponent implements OnInit {
-  public starshipsResult$!: Observable<StarshipResults>;
+  public starshipsResult$: BehaviorSubject<StarshipResults> = new BehaviorSubject<StarshipResults>({} as StarshipResults);
   public errorMessage!: string;
   public currentPage: number = 1;
   public loading: boolean = false;
+  public starshipInfo: StarshipResults | undefined;
+  showButton: boolean = true;
 
   @ViewChild('content') content!: ElementRef;
 
@@ -33,36 +29,40 @@ export class StarshipsListComponent implements OnInit {
 
   ngOnInit(): void {
     this.fetchStarships();
-
-    fromEvent(this.content.nativeElement, 'scroll')
-      .pipe(
-        catchError(() => EMPTY),
-        finalize(() => console.log('Scroll observable finalizado'))
-      )
-      .subscribe(() => {
-        if (this.shouldLoadMore()) {
-          this.currentPage++;
-          this.fetchStarships();
-        }
-      });
   }
+
   fetchStarships(): void {
     this.loading = true;
-    this.starshipsResult$ = this.starshipsService.getStarshipsList(this.currentPage).pipe(
+    this.starshipsService.getStarshipsList(this.currentPage).pipe(
       catchError((error: string) => {
         this.errorMessage = error;
         return EMPTY;
       }),
       finalize(() => this.loading = false)
-    );
+    ).subscribe((data: StarshipResults) => {
+      const currentValue = this.starshipsResult$.getValue();
+      const currentResults = currentValue.results || [];
+      const updatedResults = [...currentResults, ...data.results];
+      const updatedValue: StarshipResults = {
+        count: currentValue.count + data.count,
+        next: data.next,
+        previous: currentValue.previous,
+        results: updatedResults
+      };
+      this.starshipsResult$.next(updatedValue);
+    });
   }
 
-  shouldLoadMore(): boolean {
-    if (!this.loading) {
-      const content = this.content.nativeElement;
-      return content.scrollTop + content.clientHeight >= content.scrollHeight;
+  loadMore(): void {
+    this.currentPage++;
+    this.fetchStarships();
+    if(this.currentPage === 4) {
+      this.showButton = false;
+      return;
     }
-    return false;
   }
 
+  trackByFn(index: number, item: any): number {
+    return item.id;
+  }
 }
